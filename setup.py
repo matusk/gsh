45dfaa11e29f3923fe5db77a73632915e99990a1
Matú9a Mlynarcík <matus@mlynarcik.net>
2016-03-11 15:37:05 +0100

    create file requirement.txt create file setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gsh',
+    version='0.1',
+    packages=[''],
+    url='https://gsh.dkraic.com',
+    license='',
+    author='Daniel Kraic',
+    author_email='danielkraic@gmail.com ',
+    description='get ssh command from list of servers'
+)
